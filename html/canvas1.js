4a60d570b1ae900a09a9961933b218c59a932aa6
Francesc Via <f@gaxan.com>
2014-08-14 17:31:48 +0100

    crazy resiz

--- html/canvas1.js
+++ html/canvas1.js
@@ -1,5 +1,22 @@
 
-cnv_br = document.getElementById("braiders");
-ctx_br = cnv_br.getContext("2d");
-ctx_br.font = "bold 12px sans-serif";
-ctx_br.fillText("Braiders / Trenzadoras",10,10);
+
+
+var info = {
+    'braiders': { 'title': 'Braiders / Trenzadoras' ,'operators': [ 'trini','luque' ]  },
+    'bunching': { 'title': 'Bunching / Cableadoras' ,'operators': [ 'luis'] } , 
+    'extruding': {'title': 'Extruders / Extrusoras' ,'operators': [ 'pere']}, 
+    'deliveries': {'title': 'Deliveries / Entregas' ,'operators': [ 'carmelo']}  
+} 
+
+
+for(var propt in info){
+    cnv = document.getElementById(propt);
+    ctx = cnv.getContext("2d");
+    ctx.font = "bold 12px sans-serif";
+    ctx.fillText(info[propt]['title'],0,10);
+    ctx.fillText( info[propt]['operators'] ,0,30);
+    
+    ctx.fillRect(50, 50, 100, 100)
+
+    alert(propt + ': ' + info[propt]);
+}
